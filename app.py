b73f7c1ff78a6e02ef5f2c65080b91094dde5717
aight15 <elias.leitner@student.unisg.ch>
2025-04-02 17:25:50 +0200

    new features

--- app.py
+++ app.py
@@ -2,3 +2,6 @@ import streamlit as st
 st.write("NexTrip")
 st.write("The place where you find your next destination")
 st.write("The right destination with the right budget and right travel time")
+st.write("Personal data")
+st.text_input("Vorname")
+st.text_input("Nachname")
